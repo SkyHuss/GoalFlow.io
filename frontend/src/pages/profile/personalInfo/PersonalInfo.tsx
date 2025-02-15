ea35c654cf19ff8451d38a9e730d83ab780d3211
SkyHuss <fl.gonzales5790@gmail.com>
2025-02-15 08:45:49 +0100

    feat(front-profil): achivements icons

--- frontend/src/pages/profile/personalInfo/PersonalInfo.tsx
+++ frontend/src/pages/profile/personalInfo/PersonalInfo.tsx
@@ -1,7 +1,11 @@
+import { useUserStore } from '../../../hooks/useUserStore'
 import './PersonalInfo.css'
 
 export default function PersonalInfo() {
+
+    const {user} = useUserStore();
+
     return <div className="personal-info-container">
-        Personal info page
+        Personal info page: {user?.email}
     </div>
 }
